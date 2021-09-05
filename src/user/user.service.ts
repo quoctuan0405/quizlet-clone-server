cbab97194db30adf1bc2a415cc63c43fe641056a
Your Name <you@example.com>
2021-09-05 20:27:32 +0700

    Change sameSite: 'none' to sameSite: 'lax'

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,7 +8,7 @@ import { signJwt } from 'src/utils/jwt.utils';
 const cookieOptions: CookieOptions = {
     domain: process.env.BACKEND_DOMAIN || 'localhost',
     secure: false,
-    sameSite: 'none',
+    sameSite: 'lax',
     httpOnly: true,
     path: '/'
 };
